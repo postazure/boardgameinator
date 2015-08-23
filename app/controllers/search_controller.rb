083f67efeedb3d109e6368df7492c4baffc0c5b7
Max Gordon <gordonmaxc@gmail.com>
2015-08-23 16:19:01 -0700

    Improved Search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,11 @@ class SearchController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def search
-    boardgames = Boardgame.name_includes(params[:term])
+    boardgames = Boardgame.name_includes(normalized_search_term)
     render json: BoardgamesPresenter.new(boardgames)
+  end
+
+  def normalized_search_term
+    params['term'].gsub('+', ' ')
   end
 end
--- app/models/boardgame.rb
+++ app/models/boardgame.rb
@@ -1,5 +1,5 @@
 class Boardgame < ActiveRecord::Base
-  scope :name_includes, ->(name) { where('name iLIKE ?', "% #{name}%") }
+  scope :name_includes, ->(search_term) { where('name iLIKE ? or name iLIKE ?', "#{search_term}%", "% #{search_term}%") }
 
   validates_uniqueness_of :bgg_id
 end
